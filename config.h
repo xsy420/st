3d24f8dcd1b34089d2518ee33a7712c2b042b43d
xsy420 <1259795637@qq.com>
2022-04-19 05:17:51 +0000

    change terminal font to JetbrainsMono NF

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NF:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
