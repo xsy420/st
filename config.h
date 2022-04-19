22bb0874cf3991272ba8de269f0ed2c631784567
xsy420 <1259795637@qq.com>
2022-04-19 06:07:15 +0000

    add alpha patch

--- config.h
+++ config.h
@@ -5,8 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-//static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
-static char *font = "JetBrainsMono NF:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NF:pixelsize=15:antialias=true:autohint=true"; // default "Liberation Mono";
 static int borderpx = 2;
 
 /*
@@ -93,6 +92,9 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
+
+/* bg opacity */
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
