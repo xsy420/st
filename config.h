7c678c8206971e812c26adfc81a3ed2231b66bdd
xsy420 <1259795637@qq.com>
2023-02-07 07:08:15 +0800

    add fullscreen and scrollbacl patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NF:pixelsize=15:antialias=true:autohint=true"; // default "Liberation Mono";
+static char *font = "JetBrainsMono NF:pixelsize=22:antialias=true:autohint=true"; // default "Liberation Mono";
 static int borderpx = 2;
 
 /*
@@ -204,6 +204,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ XK_NO_MOD,            XK_F11,         fullscreen,     {.i =  0} },
+	{ MODKEY,               XK_Return,      fullscreen,     {.i =  0} },
 };
 
 /*
